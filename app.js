8df299ffa51aa14b924dea50a87e27233145b175
Edward Lee <wavefunc@gmail.com>
2017-03-19 11:11:13 +0800

    安裝 ejs-local 套件。將 classinfo 的 head 套進 layout 模板

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var engine = require('ejs-locals');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -11,6 +12,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
