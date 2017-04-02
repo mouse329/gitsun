39a536cd86d77866da17e9fc88f1904c67d57332
Edward Lee <wavefunc@gmail.com>
2017-04-02 15:15:04 +0800

    重建「所有學員查詢」頁面，重建「現任班幹部查詢」SP

--- routes/index.js
+++ routes/index.js
@@ -26,34 +26,63 @@ router.get('/classinfo', function(req, res, next) {
     }).catch(function(err) {
         //console.dir(err)
     });
- 
 });
 
-router.post('/meminfo', function(req, res, next){
+router.get('/classinfo', function(req, res, next) {
     var data = "";
-    var name = req.body.name;
     sql.connect(config).then(function() {
         new sql.Request()
-        .input('NAME', sql.NVarChar, name)
-        .execute('sp_member').then(function(recordsets) {
+        .execute('sp_ClassInfo').then(function(recordsets) {
             var data = recordsets[0]
-            //console.dir(recordsets[0])
-            //console.dir('req.body.name = '+name)
-            res.render('meminfo', { title: name, data: data })
-            res.redirect('/meminfo')
+            res.render('classinfo', { title: 'classinfo', data: data })   
         }).catch(function(err) {
-            //err
+            //console.dir(err)
         });
     }).catch(function(err) {
-        //err
+        //console.dir(err)
     });
 });
+
+router.get('/stuinfo', function(req, res, next) {
+    var data = "";
+    sql.connect(config).then(function() {
+        new sql.Request()
+        .execute('sp_StuInfo').then(function(recordsets) {
+            var data = recordsets[0]
+            res.render('stuinfo', { title: 'stuinfo', data: data })   
+        }).catch(function(err) {
+            //console.dir(err)
+        });
+    }).catch(function(err) {
+        //console.dir(err)
+    });
+});
+
+// router.post('/stuinfo', function(req, res, next){
+//     var data = "";
+//     var name = req.body.name;
+//     sql.connect(config).then(function() {
+//         new sql.Request()
+//         .input('NAME', sql.NVarChar, name)
+//         .execute('sp_StuInfo').then(function(recordsets) {
+//             var data = recordsets[0]
+//             //console.dir(recordsets[0])
+//             //console.dir('req.body.name = '+name)
+//             res.render('stuinfo', { title: name, data: data })
+//             res.redirect('/stuinfo')
+//         }).catch(function(err) {
+//             //err
+//         });
+//     }).catch(function(err) {
+//         //err
+//     });
+// });
 
 router.get('/cadreinfo', function(req, res, next) {
     var data = "";
     sql.connect(config).then(function() {
         new sql.Request()
-        .execute('proc_CadreList').then(function(recordsets) {
+        .execute('sp_CadreInfo').then(function(recordsets) {
             var data = recordsets[0]
             //console.dir(recordsets[0])
             res.render('cadreinfo', { title: 'cadreinfo', data: data })   
